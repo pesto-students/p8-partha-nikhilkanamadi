c9b314272e23161c0b68192b097e93722b62eba9
nikhilkanamadi <kanamadinikhil@gmail.com>
2023-01-21 14:14:51 +0530

    4.1 Excercise

---
+++ Week-4/Excercise 4.1/customPromise.js
@@ -0,0 +1,65 @@
+const promiseState = {
+  PENDING: "PENDING",
+  RESOLVED: "RESOLVED",
+  REJECTED: "REJECTED",
+};
+
+class CustomPromise {
+ //! constructor function to set values of promiseState
+  constructor(fn) {
+    this.promiseState = promiseState.PENDING;
+    this.resolverFunction = this.resolverFunction.bind(this);
+    this.rejectorFunction = this.rejectorFunction.bind(this);
+    this.thenFunction = null;
+    this.catchFunction = null;
+    fn(this.resolverFunction, this.rejectorFunction);
+  }
+
+  //! custom resolve function
+  resolverFunction(resolvedResponse) {
+    if (this.promiseState === promiseState.PENDING) {
+      this.thenFunction && this.thenFunction(resolvedResponse);
+    }
+    this.promiseState = promiseState.RESOLVED;
+  }
+
+  // ! custom reject function   
+  rejectorFunction(rejectResponse) {
+    if (this.promiseState === promiseState.PENDING) {
+      this.catchFunction && this.thenFunction(rejectResponse);
+    }
+    this.promiseState = promiseState.REJECTED;
+  }
+ //! custom then function   
+  then(thenFunction) {
+    this.thenFunction = thenFunction;
+    return this;
+  }
+ //! custom catch function
+  catch(catchFunction) {
+    this.catchFunction = catchFunction;
+    return this;
+  }
+}
+
+// !function to get random number 
+const getRandomNumber = () => {
+  return new CustomPromise((resolve, reject) => {
+    let randomNumber = parseInt(Math.random() * 100);
+    setTimeout(() => {
+      if (randomNumber % 5 === 0) {
+        reject(`${randomNumber} rejected.`);
+      }
+      resolve(`${randomNumber} resolved.`);
+    }, randomNumber * 10);
+  });
+};
+
+// ! calling custom method for response
+getRandomNumber()
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
